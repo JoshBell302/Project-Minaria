0d4c36b38be44e8af1f1ed05059c68f5a07e5fc2
JoshBell302 <55896779+JoshBell302@users.noreply.github.com>
2022-06-16 19:07:25 -0700

    Began Random Generator
    All aspects coded have been tested and work as intended yet the full functioning of the generator is not yet complete.

---
+++ Random DnD Generator/Random DnD Generator/main.cpp
@@ -0,0 +1,18 @@
+/*
+Author:			Joshua Bell
+Began:			June 14th 2022
+Last Updated:	June 16th 2022
+
+This is all the code I used while creating the original Dungeons and Dragons World of Minaria and 
+perhaps simillar projects in the future.
+*/
+
+#include "iostream"
+#include "randPop.h"
+
+int main() {
+	std::cout << "Hello World" << std::endl;
+	fields fullFields;
+	fullFields = fillFields();
+	return 0;
+}
